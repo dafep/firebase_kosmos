5fe0e5fa29dcc523b70922d71b4d141a5d54f8fa
Larsanov <ismail.larsanov@epitech.eu>
2020-04-19 16:54:35 +0200

    return bug

--- lib/firebase_kosmos.dart
+++ lib/firebase_kosmos.dart
@@ -13,37 +13,37 @@ class FirebaseKosmos {
   static authSignIn({String email, String password, bool debugMode, context}) async {
     FocusScope.of(context).requestFocus(FocusNode());
 
-    await Auth.signIn(
+    String e = await Auth.signIn(
       email: email,
       password: password
-    ).then((e) {
-      if (e == "OK")
-        return SUCCESS;
-      else {
-        if (debugMode == true)
-          ErrorAlert.errorAlert(e, context);
-      }
-      return e;
-    });
+    );
+
+    if (e == "OK")
+      return SUCCESS;
+    else {
+      if (debugMode == true)
+        ErrorAlert.errorAlert(e, context);
+    }
+    return e;
   }
 
   static authSignUp({String email, String password, Map<String, dynamic> map, bool debugMode, context, String cloud}) async {
     FocusScope.of(context).requestFocus(FocusNode());
 
-    await Auth.signUp(
+    String e = await Auth.signUp(
       email: email,
       password: password,
       map: map,
       cloud: cloud
-    ).then((e) {
-      if (e == "OK")
-        return SUCCESS;
-      else {
-        if (debugMode == true)
-          ErrorAlert.errorAlert(e, context);
-      }
-      return e;
-    });
+    );
+
+    if (e == "OK")
+      return SUCCESS;
+    else {
+      if (debugMode == true)
+        ErrorAlert.errorAlert(e, context);
+    }
+    return e;
   }
 
   static Future<dynamic> authLogOut() {
