cee5dc126f7cd9236f8fd7b8c847cc9f0d311d77
Larsanov <ismail.larsanov@epitech.eu>
2020-04-19 17:03:46 +0200

    return bug

--- lib/firebase_kosmos.dart
+++ lib/firebase_kosmos.dart
@@ -10,43 +10,43 @@ class FirebaseKosmos {
 
   /* Auth */
 
-  Future<dynamic> authSignIn({String email, String password, bool debugMode, context}) async {
+  static Future<dynamic> authSignIn({String email, String password, bool debugMode, context}) async {
     FocusScope.of(context).requestFocus(FocusNode());
 
-    String e = await Auth.signIn(
+    final response = await Auth.signIn(
       email: email,
       password: password
     );
 
-    if (e == "OK")
+    if (response == "OK")
       return SUCCESS;
     else {
       if (debugMode == true)
-        ErrorAlert.errorAlert(e, context);
+        ErrorAlert.errorAlert(response, context);
+      return response;
     }
-    return e;
   }
 
-  Future<dynamic> authSignUp({String email, String password, Map<String, dynamic> map, bool debugMode, context, String cloud}) async {
+  static Future<dynamic> authSignUp({String email, String password, Map<String, dynamic> map, bool debugMode, context, String cloud}) async {
     FocusScope.of(context).requestFocus(FocusNode());
 
-    String e = await Auth.signUp(
+    final response = await Auth.signUp(
       email: email,
       password: password,
       map: map,
       cloud: cloud
     );
 
-    if (e == "OK")
+    if (response == "OK")
       return SUCCESS;
     else {
       if (debugMode == true)
-        ErrorAlert.errorAlert(e, context);
+        ErrorAlert.errorAlert(response, context);
+      return response;
     }
-    return e;
   }
 
-  static Future<dynamic> authLogOut() {
+  static authLogOut() {
     return Auth.logOutAuth();
   }
 
